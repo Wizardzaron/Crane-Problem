01c8eaee0d103d94f0c9cd0e555e9e7023461f99
Wizardzaron <mochi456@csu.fullerton.edu>
2023-04-28 16:11:18 -0700

    project

---
+++ cranes_algs.hpp
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////////////////////////
+// cranes_algs.hpp
+//
+// Algorithms that solve the crane unloading problem.
+//
+// All of the TODO sections for this project reside in this file.
+//
+// This file builds on crane_types.hpp, so you should familiarize yourself
+// with that file before working on this file.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <cassert>
+#include <math.h>
+
+#include "cranes_types.hpp"
+
+namespace cranes {
+
+// Solve the crane unloading problem for the given grid, using an exhaustive
+// optimization algorithm.
+//
+// This algorithm is expected to run in exponential time, so the grid's
+// width+height must be small enough to fit in a 64-bit int; this is enforced
+// with an assertion.
+//
+// The grid must be non-empty.
+path crane_unloading_exhaustive(const grid& setting) {
+
+  // grid must be non-empty.
+  assert(setting.rows() > 0);
+  assert(setting.columns() > 0);
+
+  // Compute maximum path length, and check that it is legal.
+  const size_t max_steps = setting.rows() + setting.columns() - 2;
+  assert(max_steps < 64);
+
+  // TODO: implement the exhaustive search algorithm, then delete this
+  // comment.
+  path best(setting);
+  for (size_t steps = 0; steps <= max_steps; steps++) {
+}
+
+// Solve the crane unloading problem for the given grid, using a dynamic
+// programming algorithm.
+//
+// The grid must be non-empty.
+//path crane_unloading_dyn_prog(const grid& setting) {
+path crane_unloading_dyn_prog(const grid& setting) {
+
+  // grid must be non-empty.
+  assert(setting.rows() > 0);
+  assert(setting.columns() > 0);
+
+  // TODO: implement the dynamic programming algorithm, then delete this
+  // comment.
+
+
+
+   assert(best->has_value());
+//  //   std::cout << "total cranes" << (**best).total_cranes() << std::endl;
+
+   return **best;
+	}
+
+}
