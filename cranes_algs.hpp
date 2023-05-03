f1fdc42bc0bed3f3be076da9d43e66f87018b8b8
Noah Khayat <naeemalbirkdar@aol.com>
2023-05-02 17:20:29 -0700

    initial update

--- cranes_algs.hpp
+++ cranes_algs.hpp
@@ -17,52 +17,74 @@
 
 #include "cranes_types.hpp"
 
-namespace cranes {
+namespace cranes 
+{
 
-// Solve the crane unloading problem for the given grid, using an exhaustive
-// optimization algorithm.
-//
-// This algorithm is expected to run in exponential time, so the grid's
-// width+height must be small enough to fit in a 64-bit int; this is enforced
-// with an assertion.
-//
-// The grid must be non-empty.
-path crane_unloading_exhaustive(const grid& setting) {
+  // Solve the crane unloading problem for the given grid, using an exhaustive
+  // optimization algorithm.
+  //
+  // This algorithm is expected to run in exponential time, so the grid's
+  // width+height must be small enough to fit in a 64-bit int; this is enforced
+  // with an assertion.
+  //
+  // The grid must be non-empty.
+  path crane_unloading_exhaustive(const grid& setting) 
+  {
 
-  // grid must be non-empty.
-  assert(setting.rows() > 0);
-  assert(setting.columns() > 0);
+    // grid must be non-empty.
+    assert(setting.rows() > 0);
+    assert(setting.columns() > 0);
 
-  // Compute maximum path length, and check that it is legal.
-  const size_t max_steps = setting.rows() + setting.columns() - 2;
-  assert(max_steps < 64);
+    // Compute maximum path length, and check that it is legal.
+    const size_t max_steps = setting.rows() + setting.columns() - 2;
+    assert(max_steps < 64);
 
-  // TODO: implement the exhaustive search algorithm, then delete this
-  // comment.
-  path best(setting);
-  for (size_t steps = 0; steps <= max_steps; steps++) {
-}
+    // TODO: implement the exhaustive search algorithm, then delete this
+    // comment.
 
-// Solve the crane unloading problem for the given grid, using a dynamic
-// programming algorithm.
-//
-// The grid must be non-empty.
-//path crane_unloading_dyn_prog(const grid& setting) {
-path crane_unloading_dyn_prog(const grid& setting) {
 
-  // grid must be non-empty.
-  assert(setting.rows() > 0);
-  assert(setting.columns() > 0);
+
+    path best(setting);
+
+    for (size_t steps = 0; steps <= max_steps; steps++) 
+    {
+
+      
+
+
+
+    }
+
+
+
+
+
+
+  }
+
+  // Solve the crane unloading problem for the given grid, using a dynamic
+  // programming algorithm.
+  //
+  // The grid must be non-empty.
+  //path crane_unloading_dyn_prog(const grid& setting) {
+  path crane_unloading_dyn_prog(const grid& setting) 
+  {
+
+    // grid must be non-empty.
+    assert(setting.rows() > 0);
+    assert(setting.columns() > 0);
 
   // TODO: implement the dynamic programming algorithm, then delete this
   // comment.
 
 
 
-   assert(best->has_value());
-//  //   std::cout << "total cranes" << (**best).total_cranes() << std::endl;
+    assert(best->has_value());
+  //  //   std::cout << "total cranes" << (**best).total_cranes() << std::endl;
 
-   return **best;
-	}
+    return **best;
+  
+  }
 
 }
+
